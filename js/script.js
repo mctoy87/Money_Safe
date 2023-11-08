7228195ef45f613ae68dcc6a7fffa7f9411b6769
mctoy87 <mctoy87@mail.ru>
2023-11-08 18:34:09 +0200

    added API server, added the ability to receive and display data from the server in a Report, and filter by date

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
 // получим функцию проверки числа
 import {convertStringNumber} from './convertStringNumber.js';
+// импорт из библиотеки OverlayScrollbars для кастомизации скроллбара
+import {OverlayScrollbars} from './overlayscrollbars.esm.min.js';
+
+//адрес сервера с glitch
+const API_URL = 'https://principled-lush-tortellini.glitch.me/api';
+
+// зададим тип операции (приход или уход денег в Отчете)
+const typeOperation = {
+  income: "доход",
+  expenses: "расход",
+};
 
 // получим форму
 const financeForm = document.querySelector('.finance__form');
 // получим сумму кошелька
 const financeAmount = document.querySelector('.finance__amount');
 // получим кнопку Отчет
-const openReport = document.querySelector('.finance__report');
+const financeReport = document.querySelector('.finance__report');
 // получим окно Отчет
 const report = document.querySelector('.report');
-// получим крестик на окне Отчет
-const closeReport = document.querySelector('.report__close');
-
+// получим tbody
+const reportOperationList = document.querySelector('.report__operation-list');
+// получим форму филтрации отчета по дате
+const reportDates = document.querySelector('.report__dates');
 
 // создадим счетчик с текущим значением кошелька
 let amount = 0;
@@ -46,18 +58,149 @@ financeForm.addEventListener('submit', (event) => {
   financeAmount.textContent = `${amount.toLocaleString()} ₽`;
 });
 
-// слушаем событие клика на кнопку открыть Отчет
-openReport.addEventListener('click', (e) => {
-  e.preventDefault();
+// ф-я закрытия Отчета
+// деструктуируем и вытаскиваем сразу таргет
+const closeReport = ({ target }) => {
+  
+  // если клик на крестик или не не по Отчету
+  if (
+     // если клик на кнопку крестик
+    target.closest('.report__close') ||
+    // если клик мимо Отчета и это не кнопка Открыть отчет
+    // клик по кнопке Открыть м.б. т.к. событие и таргет один у ф-и откр и закр Отчета
+    (!target.closest('.report') && target !== financeReport)
+  ) {
+    // то закрываем Отчет
+    report.classList.remove('report__open'); 
+    // убираем слушателя всего документа
+    document.removeEventListener('click', closeReport)
+  }
+};
+
+// функция открыть Отчет
+const openReport = () => {
   // добавим класс Отчету чтобы открыть его
   report.classList.add('report__open');
+
+  // слушаем клик по крестику для закрытия Отчета
+  document.addEventListener('click', closeReport)
+};
+
+// получает данные с сервера
+const getData = async (url) => {
+  try {
+    // делаем запрос к API(серверу) по переданному url
+    const response = await fetch(`${API_URL}${url}`);
+    // проверим статус ответа от сервера и если false
+    if (!response.ok) {
+      // создаем сами ошибку и передаем код ответа сервера
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    // если true, то вернем данные в формате json
+    return await response.json();
+  } catch (error) {
+    console.error('Ошибка при получении данных: ', error);
+    // если будут ошибки которые catch не поймет
+    throw error;
+  }
+};
+
+// изменяет формат даты на нужный
+const reformatDate = (dateStr) => {
+  // деструктуируем dateStr с помощью split, кот. создаст массив разделяя по дефису
+  const [year, mounth, day] = dateStr.split('-');
+  // соберем дату в нужном формате
+  // padStart добавит второй символ если число не двухзначное
+  return `${day.padStart(2, "0")}.${mounth.padStart(2, "0")}.${year}`
+};
+
+// ф-я рендерит данные
+const renderReport = (data) => {
+  // очищаем tbody таблицу
+  reportOperationList.textContent = '';
+
+  // формируем строки таблицы
+  // деструктуируем каждую операцию в data 
+  const reportRows = data.map(
+    ({category, amount, description, date, type}) => {
+      // создаем новую строку 
+      const reportRow = document.createElement('tr');
+      // добавим ей класс
+      reportRow.classList.add('report__row');
+      // вставить в эту строку все что нужно из верстки, добавив данные из data
+      reportRow.innerHTML = `
+        <td class="report__cell">${category}</td>
+        <td class="report__cell">${amount.toLocaleString()}&nbsp₽</td>
+        <td class="report__cell">${description}</td>
+        <td class="report__cell">${reformatDate(date)}</td>
+        <td class="report__cell">${typeOperation[type]}</td>
+        <td class="report__action-cell">
+          <button
+            class="report__button report__button_table">&#10006;</button>
+        </td>
+      `;
+      // вернем сформированную строку в reportRows
+      return reportRow;
+  });
+
+  // вставим в tbody таблицы все сформированные строки
+  reportOperationList.append(...reportRows);
+};
+
+// слушаем событие клика на кнопку открыть Отчет
+financeReport.addEventListener('click', async () => {
+  // открыть Отчет через ф-ю
+  openReport();
+  // запрос к серверу и получение данных
+  const data = await getData("/test");
+
+  // отобразить данные с сервера
+  renderReport(data);
 });
+
 // слушаем событие клика на Отчет
 report.addEventListener('click', (e) => {
   // если кликнули на крестик то закрываем окно
   if(e.target.closest('.report__close')) {
     report.classList.remove('report__open');
   }
+});
+
+// кастомизируем скроллбар
+OverlayScrollbars(report, {});
+
+// слушатель на фильтр внутри отчета
+reportDates.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  // получить данные в формате formdata из формы фильтра 
+  const formData = Object.fromEntries(new FormData(reportDates));
+
+  // создаем объект search-параметрами
+  const searchParams = new URLSearchParams();
+
+  // проверяем есть ли в FormData наши параметры поиска
+  if (formData.startDate) {
+    // если есть, то добавим в search параметры 
+    searchParams.append('startDate', formData.startDate)
+  };
+
+  // проверяем есть ли в FormData наши параметры поиска
+  if (formData.endDate) {
+    // если есть, то добавим в search параметры 
+    searchParams.append('endDate', formData.endDate)
+  };
+  // формируем queryString, т.е. одну строку со всеми query параметрами
+  const queryString = searchParams.toString();
+
+  // формирует url в зависимости есть query-параметры или если нет
+  const url = queryString ? `/test?${queryString}` : '/test';
+  
+  // запрос к серверу и получение данных с search-параметрами
+  const data = await getData(url);
+
+  // отобразить данные с сервера
+  renderReport(data);
 });
 
 
